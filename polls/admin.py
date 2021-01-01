275998917e9529037ab07fb93ec522a719bf0b6d
Otabek <o.latibov@gmail.com>
2021-01-01 20:08:06 +0500

    check project for git

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+# Register your models here.
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text', 'file']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':['collapse']})
+
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
---
+++ polls/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+from django.core.files.storage import FileSystemStorage
+
+fs = FileSystemStorage(location='./polls/archive/polls/')
+
+
+# Create your models here.
+
+class Question(models.Model):
+    file = models.FileField(max_length=100, default='DEFAULT VALUE', storage=fs)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date published')
+
+    def loaded_file(self):
+        return self.file
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
